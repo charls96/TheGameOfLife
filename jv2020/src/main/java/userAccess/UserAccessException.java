7289a189531db08bbc3bd8dd4ff6a885547dd82e
Agustín Juan Pérez <agu.ple@gmail.com>
2021-05-16 18:53:14 +0000

    Merged in 4-Excepciones (pull request #7)
    Nuevas clases runtimeException

---
+++ jv2020/src/main/java/userAccess/UserAccessException.java
@@ -0,0 +1,13 @@
+package userAccess;
+
+public class UserAccessException extends RuntimeException {
+
+	public UserAccessException() {
+		super();
+	}
+
+	public UserAccessException(String message) {
+		super(message);
+	}
+
+}
