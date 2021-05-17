605ddb6879aca345e5fa9e31bddb7d1a9f8a0b7a
Agustín Juan Pérez <agu.ple@gmail.com>
2021-05-17 20:49:44 +0000

    Merged in 6-DAOs_memoria (pull request #11)
    6 DAOs memoria
    
    * Interface de métodos CRUD para los DAOs
    * DAOs de memoria con template method indexSort()

---
+++ jv2020/src/main/java/dataAccess/memory/IndexSortTemplate.java
@@ -0,0 +1,34 @@
+package dataAccess.memory;
+
+import java.util.List;
+
+import models.Identifiable;
+
+public abstract class IndexSortTemplate {
+
+	/**
+	 *  Obtiene por búsqueda binaria, la posición que ocupa, o ocuparía,  un objeto en la estructura.
+	 *	@param Id - id del objeto a buscar.
+	 * 	@param data - Estructura de datos con elementos identificables (poseen un id utilizado para la búsqueda y ordenación).
+	 *	@return - la posición, en base 1, que ocupa un objeto o la que ocuparía (negativo).
+	 */
+	protected int indexSort(List<Identifiable> data, String id) {
+		int start = 0;
+		int end = data.size() - 1;
+		while (start <= end) {
+			int half = (start + end) / 2;	
+			int comparison = data.get(half).getId().compareTo(id);		
+			if (comparison == 0) {
+				return half + 1; 		// Indices base 1
+			}
+			if (comparison < 0) {
+				start = half + 1;
+			} 
+			else {
+				end = half - 1;
+			}
+		}
+		return -(end + 2); 				// Indices base 1
+	}
+
+} 
---
+++ jv2020/src/main/java/dataAccess/memory/SimulationsDAO.java
@@ -0,0 +1,91 @@
+package dataAccess.memory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import dataAccess.DataAccessException;
+import dataAccess.OperationsDAO;
+import models.Identifiable;
+import models.User;
+
+public class SimulationsDAO extends IndexSortTemplate implements OperationsDAO {
+
+	private ArrayList<Identifiable> simulationsData;
+	
+	private static SimulationsDAO instance;
+	
+	private SimulationsDAO() {		
+		this.simulationsData = new ArrayList<Identifiable>();
+	}
+	
+	public static SimulationsDAO getInstance() {
+		if (instance == null) {
+			instance = new SimulationsDAO();
+		}
+		return instance;
+	}
+	
+	@Override
+	public Identifiable find(String id) {
+		int pos = this.indexSort(this.simulationsData, id);
+		if (pos >= 0) {
+			return (User) this.simulationsData.get(pos);
+		}
+		return null;
+	}
+
+	@Override
+	public List<Identifiable> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void create(Identifiable simulation) throws DataAccessException {
+		assert simulation != null;
+		int insertionPos = indexSort(this.simulationsData, simulation.getId()); 
+		if (insertionPos < 0) {
+			this.simulationsData.add(Math.abs(insertionPos)-1, simulation);
+			return;
+		}
+		throw new DataAccessException("simulationsDAO.create: "+ simulation.getId() + " ya existe");	
+	}
+
+	@Override
+	public Identifiable delete(String id) throws DataAccessException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Identifiable delete(Identifiable obj) throws DataAccessException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Identifiable update(Identifiable obj) throws DataAccessException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String toStringDatos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String toStringIds() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteAll() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
