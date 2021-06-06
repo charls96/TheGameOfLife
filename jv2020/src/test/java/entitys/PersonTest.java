601070ae0b3ef8d0aab77f0022c3d5355050941b
Juanjo M.M <juanjomartinezmiranda@gmail.com>
2021-06-06 11:19:45 +0000

    PersonTest.java  corregido

--- jv2020/src/test/java/entitys/PersonTest.java
+++ jv2020/src/test/java/entitys/PersonTest.java
@@ -19,7 +19,10 @@ public class PersonTest {
         Nif nif = new Nif("31405204S");
         Address address = new Address("Calle Java", "Nº 1", "30001", "Murcia");
         Mail mail = new Mail("luisgarcia@gmail.com");
-        EasyDate dob = new EasyDate(1990, 1, 1);
-        personTest1 = new Person(nif, "Luis", "García Rodríguez", address, mail, dob);
+        EasyDate birthDate = new EasyDate(1990, 1, 1);
+        EasyDate registeredDate = new EasyDate(2021, 1, 1);
+        Password password = new Password("abcdefgh");
+        personTest1 = new User(nif, "Luis", "García Rodríguez", address, mail, birthDate,
+                registeredDate, password, User.RoleUser.REGISTERED);
     }
-} 
+}
