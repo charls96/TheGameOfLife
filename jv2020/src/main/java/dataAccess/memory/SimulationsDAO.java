bbf9bcba25d735656d68200c7b5fbbeb076318be
albertojcr <albertojcr96@gmail.com>
2021-06-07 19:09:02 +0200

    Implementación deleteAll()

--- jv2020/src/main/java/dataAccess/memory/SimulationsDAO.java
+++ jv2020/src/main/java/dataAccess/memory/SimulationsDAO.java
@@ -110,8 +110,7 @@ public class SimulationsDAO extends IndexSortTemplate implements OperationsDAO {
 
 	@Override
 	public void deleteAll() {
-		// TODO Auto-generated method stub
-		
+		simulationsData.clear();
 	}
 
 
