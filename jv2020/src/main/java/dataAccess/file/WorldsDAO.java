3ba502de066906b3fe2386f58a0d960a4b50a16e
Rosigator <heccas@gmail.com>
2021-06-09 01:44:13 +0200

    --

--- jv2020/src/main/java/dataAccess/file/WorldsDAO.java
+++ jv2020/src/main/java/dataAccess/file/WorldsDAO.java
@@ -77,7 +77,7 @@ public class WorldsDAO extends IndexSortTemplate implements OperationsDAO, Persi
 		}
 	}
 	
-	//OPERACIONES DAO
+	//OPERACIONES DAO.
 	
 	@Override
 	public World find(String id) {	
@@ -125,7 +125,7 @@ public class WorldsDAO extends IndexSortTemplate implements OperationsDAO, Persi
 
 	@Override
 	public Identifiable update(Identifiable world) throws DataAccessException {
-		// TODO Auto-generated method stub
+		// TODO
 		return null;	
 	}
 
