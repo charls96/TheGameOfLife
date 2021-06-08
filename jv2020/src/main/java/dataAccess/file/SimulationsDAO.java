71de85186f0445f096d648e9b4a8100cf7ab67d9
Rosigator <heccas@gmail.com>
2021-06-08 17:50:59 +0200

    Relleno de método toStringIds, modificación de error en toStringData

--- jv2020/src/main/java/dataAccess/file/SimulationsDAO.java
+++ jv2020/src/main/java/dataAccess/file/SimulationsDAO.java
@@ -143,16 +143,19 @@ public class SimulationsDAO extends IndexSortTemplate implements OperationsDAO, 
 	@Override
 	public String toStringData() {	
 		StringBuilder textData = new StringBuilder();
-		for (Identifiable sesion: simulationsData) {
-			textData.append("\n" + sesion); 
+		for (Identifiable simulation: this.findAll()) {
+			textData.append("\n" + simulation); 
 		}
 		return textData.toString();
 	}
 
 	@Override
 	public String toStringIds() {	
-		// TODO Auto-generated method stub
-		return null;
+		StringBuilder textData = new StringBuilder();
+		for (Identifiable simulation: this.findAll()) {
+			textData.append("\n" + simulation.getId()); 
+		}
+		return textData.toString();
 	}
 
 	@Override
