578b4eb8ee2670bd2fd4bd579bcaaae9fb486284
Alex López <alexingenierocierva@gmail.com>
2021-06-15 09:42:44 +0000

    Implementación sessionUserFilter

--- jv2020/src/main/java/dataAccess/memory/SessionsDAO.java
+++ jv2020/src/main/java/dataAccess/memory/SessionsDAO.java
@@ -37,6 +37,20 @@ public class SessionsDAO extends IndexSortTemplate implements OperationsDAO {
 		return this.sessionsData.subList(firstIndex, lastIndex + 1);
 	}
 	
+	public List<Identifiable> findAllUser(String userId) {	
+	
+		String LastSessionId = userId + ":" + new EasyDate().toStringTimeStamp();
+		int index = indexSort(this.sessionsData, LastSessionId);
+		
+		if (index > 0) {
+			return sessionsUserFilter(indexSort(this.sessionsData, LastSessionId) - 1);
+		}	
+		
+		return null;
+		
+	}
+
+	
 	@Override
 	public Identifiable find(String id) {
 		int pos = this.indexSort(this.sessionsData, id);
